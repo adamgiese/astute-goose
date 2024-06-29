eb52e1142b771cb4ab66fab48bdfe2bb7ed1f17f
Adam Giese <adam.giese@gmail.com>
2024-06-28 21:52:23 -0500

    Adjust tagline

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Game Ideas",
 	url: "https://gameideas.net/",
 	language: "en",
-	description: "Ideas about and for games",
+	description: "Ideas about and for board games",
 	author: {
 		name: "Adam Giese",
 		email: "adam.giese@gmail.com",
